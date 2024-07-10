40c92420dbbcaf84171d82cb35d7bdc319090f7f
SSG <thsdyek@gmail.com>
2024-07-10 23:08:18 +0900

    HPBar 데미지 효과 만들기

--- Assets/Script/PlayerHPBar.cs
+++ Assets/Script/PlayerHPBar.cs
@@ -10,26 +10,45 @@ public class PlayerHPBar : MonoBehaviour
     public Transform player;
     public Transform tr;
     public Slider slider;
+    public Slider sliderBack;
     public float maxHP;
-    public float currentHP;
+    private float currentHP;
     float unitHP = 200;
     public HorizontalLayoutGroup hpLineFoler;
+    public Vector3 correctPos;
+    
 
     float lastMaxHP = 0;
-
+    bool backHPSlideAni = false;
     private void Start()
     {
-        lastMaxHP = maxHP;
+        currentHP = lastMaxHP = maxHP;
+        backHPSlideAni = false;
+        sliderBack.value =  slider.value = 1;
+
     }
     // Update is called once per frame
     void Update()
     {
-        Vector3 vec = player.position;
-        vec.y = 0;
+        Vector3 vec = player.position + correctPos;
         tr.position = vec;
 
-        slider.value = currentHP / maxHP;
         updateHpLine = lastMaxHP != maxHP;
+
+        // 자연스럽게 감소
+        slider.value = Mathf.Lerp(slider.value, currentHP / maxHP, Time.deltaTime * 5f);
+
+        if (backHPSlideAni)
+        {
+            sliderBack.value = Mathf.Lerp(sliderBack.value, slider.value, Time.deltaTime * 5f);
+            if (slider.value >= sliderBack.value - 0.01f)
+            {
+                backHPSlideAni = false;
+                sliderBack.value = slider.value;
+            }
+                
+        }
+            
     }
 
 
@@ -52,5 +71,16 @@ public class PlayerHPBar : MonoBehaviour
             child.gameObject.transform.localScale = vec;
         }
         hpLineFoler.enabled = true;
+    }
+
+    public void OnDmg(int dmg)
+    {
+        currentHP -= dmg;
+        Invoke("BackHPFun", 0.5f);
+    }
+
+    public void BackHPFun()
+    {
+        backHPSlideAni = true;
     }
 }
